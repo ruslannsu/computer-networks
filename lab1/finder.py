b2667f8412163414b64e127facc3bdfcb7a30dd0
ruslan <ruslanone57@gmail.com>
2025-09-16 20:21:13 +0700

    add: config

--- lab1/finder.py
+++ lab1/finder.py
@@ -1,15 +1,22 @@
 import struct
 import time 
 from socket import *
-from threading import *
-from collections import deque
+import yaml
 
-import collections
+
+
+
 class Finder(object):
     def __init__(self):
-        self.multicast_group = ('224.1.1.1', 5000)
-        self.message = '12242232'
-        self.enc = 'utf-8'
+
+        with open('config.yaml', 'r') as f:
+            config = yaml.safe_load(f)
+
+        self.multicast_group = (config['multicast']['group'], config['multicast']['port'])
+        self.message = config['multicast']['message']
+        self.enc = config['multicast']['encoding']
+        self.time_offset = config['time']['offset']
+        self.buffer_size = config['network']['buffer_size']
         self.send_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
         self.send_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         self.send_socket.setsockopt(IPPROTO_IP, IP_MULTICAST_TTL, 2)
@@ -19,39 +26,35 @@ class Finder(object):
         mreq = struct.pack("4s4s", inet_aton(self.multicast_group[0]), inet_aton('0.0.0.0'))
         self.rec_socket.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
         self.dict = {}
-        self.time_offset = 5
         
-
     def clean(self):
         time_ = int(time.time())
         for key, value in list(self.dict.items()):
             if (time_ - value > self.time_offset):
                 self.dict.pop(key)
                 print(f"{key} died")
+                self.screen()
 
-
-     
     def screen(self):
-        for _, value in self.dict.items():
-            print('alive:')
-            print(value)    
-        
-            
-        
-
+        print('alive')
+        for key, _ in self.dict.items():
+            print(key)    
 
     def run(self):
-        self.screen()
         while (True):
             send_message = self.message.encode(self.enc)
             self.send_socket.sendto(send_message, self.multicast_group)
-            message, address = self.rec_socket.recvfrom(1024)
+            message, address = self.rec_socket.recvfrom(self.buffer_size)
             message = message.decode(self.enc)
             if (message != self.message):
                 continue
+            if (not(address in self.dict)):
+                print(f"new: {address}")
+                self.screen()
+                
             self.dict[address] = time.time()
-            time.sleep(1)
             self.clean()
+            
 
 
             
