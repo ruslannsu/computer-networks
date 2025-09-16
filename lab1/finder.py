cf05c44d4cb54e453d9567de6559573928d008ab
ruslan <ruslanone57@gmail.com>
2025-09-16 19:48:10 +0700

    fix: threads removed add: dict

--- lab1/finder.py
+++ lab1/finder.py
@@ -2,12 +2,13 @@ import struct
 import time 
 from socket import *
 from threading import *
+from collections import deque
 
-
+import collections
 class Finder(object):
     def __init__(self):
         self.multicast_group = ('224.1.1.1', 5000)
-        self.message = 'hello world'
+        self.message = '12242232'
         self.enc = 'utf-8'
         self.send_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
         self.send_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -17,27 +18,45 @@ class Finder(object):
         self.rec_socket.bind(('', self.multicast_group[1]))
         mreq = struct.pack("4s4s", inet_aton(self.multicast_group[0]), inet_aton('0.0.0.0'))
         self.rec_socket.setsockopt(IPPROTO_IP, IP_ADD_MEMBERSHIP, mreq)
+        self.dict = {}
+        self.time_offset = 5
+        
 
-    def multicast_send(self):
-        while(True):
-            send_message = self.message.encode(self.enc)
-            self.send_socket.sendto(send_message, self.multicast_group)
-            time.sleep(1)
+    def clean(self):
+        time_ = int(time.time())
+        for key, value in list(self.dict.items()):
+            if (time_ - value > self.time_offset):
+                self.dict.pop(key)
+                print(f"{key} died")
 
-    def multicast_receive(self):
-        while(True):
-            data, addr = self.rec_socket.recvfrom(1024)
-            print(data.decode(self.enc))
-            print(addr)
-            time.sleep(1)
+
+     
+    def screen(self):
+        for _, value in self.dict.items():
+            print('alive:')
+            print(value)    
+        
+            
+        
 
 
     def run(self):
-        rec_thread = Thread(target=self.multicast_receive)
-        send_thread = Thread(target=self.multicast_send)
-        rec_thread.start()
-        send_thread.start()
+        self.screen()
+        while (True):
+            send_message = self.message.encode(self.enc)
+            self.send_socket.sendto(send_message, self.multicast_group)
+            message, address = self.rec_socket.recvfrom(1024)
+            message = message.decode(self.enc)
+            if (message != self.message):
+                continue
+            self.dict[address] = time.time()
+            time.sleep(1)
+            self.clean()
 
 
-f = Finder()
-f.run()
+            
+app = Finder()
+
+            
+while(True):
+    app.run()
--- lab1/main.py
+++ lab1/main.py
@@ -1,19 +1,9 @@
 import threading
+import time
+print(time.time())
 
 
-def printer(string):
-    while (1):
-      print(string)
-      
+num = 140239423
 
 
-
-thread1 = threading.Thread(target=printer, args=('hello world', ))
-
-
-thread2 = threading.Thread(target=printer, args=('hello thread', ))
-
-
-thread1.start()
-thread2.start()
-    
+print(f"{num} dead")
